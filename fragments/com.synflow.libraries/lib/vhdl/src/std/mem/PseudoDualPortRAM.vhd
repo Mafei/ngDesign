9858028d457cbe58195b39b5ea0bdd77cf3924c7
Matthieu Wipliez <matthieu.wipliez@synflow.com>
2014-08-25 10:46:13 +0200

    updated VHDL RAM entities (true dual and pseudo dual)

--- fragments/com.synflow.libraries/lib/vhdl/src/std/mem/PseudoDualPortRAM.vhd
+++ fragments/com.synflow.libraries/lib/vhdl/src/std/mem/PseudoDualPortRAM.vhd
@@ -11,7 +11,7 @@
 -------------------------------------------------------------------------------
 
 -------------------------------------------------------------------------------
--- Title      : Dual-port inferred RAM
+-- Title      : Pseudo dual-port inferred RAM
 -- Author     : Nicolas Siret (nicolas.siret@synflow.com)
 --              Matthieu Wipliez (matthieu.wipliez@synflow.com)
 -- Standard   : VHDL'93
@@ -27,25 +27,18 @@ use ieee.numeric_std.all;
 -------------------------------------------------------------------------------
 -- Entity
 -------------------------------------------------------------------------------
-entity DualPortRAM is
+entity PseudoDualPortRAM is
   generic (
     depth : integer := 8;
     width : integer := 16;
     numAdditionalRegisters : integer := 0);
   port (
-    clock_a, clock_b : in std_logic;
-    --
-    address_a   : in std_logic_vector(depth - 1 downto 0);
-    data_a      : in std_logic_vector(width - 1 downto 0);
-    data_a_send : in std_logic;
-    --
-    address_b   : in std_logic_vector(depth - 1 downto 0);
-    data_b      : in std_logic_vector(width - 1 downto 0);
-    data_b_send : in std_logic;
-    --
-    q_a : out std_logic_vector(width - 1 downto 0);
-    q_b : out std_logic_vector(width - 1 downto 0));
-end DualPortRAM;
+    rd_clock, wr_clock     : in  std_logic;
+    rd_address, wr_address : in  std_logic_vector(depth - 1 downto 0);
+    data                   : in  std_logic_vector(width - 1 downto 0);
+    data_send              : in  std_logic;
+    q                      : out std_logic_vector(width - 1 downto 0));
+end PseudoDualPortRAM;
 -------------------------------------------------------------------------------
 
 
@@ -53,7 +46,7 @@ end DualPortRAM;
 -------------------------------------------------------------------------------
 -- Architecture
 -------------------------------------------------------------------------------
-architecture rtl_DualPortRAM of DualPortRAM is
+architecture rtl_PseudoDualPortRAM of PseudoDualPortRAM is
 
   -----------------------------------------------------------------------------
   -- RAM contents
@@ -64,28 +57,20 @@ architecture rtl_DualPortRAM of DualPortRAM is
 -------------------------------------------------------------------------------
 begin
 
-  access_a: process(clock_a)
+  readData : process (rd_clock)
   begin
-    if rising_edge(clock_a) then
-      if data_a_send = '1' then
-        ram(to_integer(unsigned(address_a))) := data_a;
-        q_a <= data_a;
-      else
-        q_a <= ram(to_integer(unsigned(address_a)));
+    if rising_edge(rd_clock) then
+      q <= ram(to_integer(unsigned(rd_address)));
+    end if;
+  end process readData;
+
+  writeData : process (wr_clock)
+  begin
+    if rising_edge(wr_clock) then
+      if data_send = '1' then
+        ram(to_integer(unsigned(wr_address))) := data;
       end if;
     end if;
-  end process access_a;
+  end process writeData;
 
-  access_b: process(clock_b)
-  begin
-    if rising_edge(clock_b) then
-      if data_b_send = '1' then
-        ram(to_integer(unsigned(address_b))) := data_b;
-        q_b <= data_b;
-      else
-        q_b <= ram(to_integer(unsigned(address_b)));
-      end if;
-    end if;
-  end process access_b;
-
-end rtl_DualPortRAM;
+end rtl_PseudoDualPortRAM;
