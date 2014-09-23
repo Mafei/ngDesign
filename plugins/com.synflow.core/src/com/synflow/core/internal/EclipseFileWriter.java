474bb9eac2ef40558f9ac725cf0c0fc76b402068
Matthieu Wipliez <matthieu.wipliez@synflow.com>
2014-09-23 12:07:38 +0200

    handle case change for generated files on case insensitive file systems

--- plugins/com.synflow.core/src/com/synflow/core/internal/EclipseFileWriter.java
+++ plugins/com.synflow.core/src/com/synflow/core/internal/EclipseFileWriter.java
@@ -13,14 +13,15 @@ package com.synflow.core.internal;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 
 import com.synflow.core.IFileWriter;
 import com.synflow.core.SynflowCore;
@@ -34,54 +35,22 @@ import com.synflow.core.SynflowCore;
  */
 public class EclipseFileWriter implements IFileWriter {
 
+	private IProject project;
+
 	/**
-	 * If it does not exist, creates the given folder. If the parent folders do not exist either,
-	 * create them.
+	 * Taken from org.eclipse.xtext.builder.EclipseResourceFileSystemAccess2, (c) 2011 itemis AG
 	 * 
-	 * @param folder
-	 *            a folder
+	 * @param container
 	 * @throws CoreException
 	 */
-	public static void createFolder(IFolder folder) throws CoreException {
-		IPath path = folder.getFullPath();
-		if (folder.exists()) {
+	private void ensureExists(IContainer container) throws CoreException {
+		if (container.exists()) {
 			return;
-		}
-
-		int n = path.segmentCount();
-		if (n < 2) {
-			throw new IllegalArgumentException("the path of the given folder "
-					+ "must have at least two segments");
-		}
-
-		// check project
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IProject project = root.getProject(path.segment(0));
-		if (!project.exists()) {
-			project.create(null);
-		}
-
-		// open project
-		if (!project.isOpen()) {
-			project.open(null);
-		}
-
-		// check folder
-		folder = root.getFolder(path.uptoSegment(2));
-		if (!folder.exists()) {
-			folder.create(true, true, null);
-		}
-
-		// and then check all the descendants
-		for (int i = 2; i < n; i++) {
-			folder = folder.getFolder(new Path(path.segment(i)));
-			if (!folder.exists()) {
-				folder.create(true, true, null);
-			}
+		} else if (container instanceof IFolder) {
+			ensureExists(container.getParent());
+			((IFolder) container).create(true, true, null);
 		}
 	}
-
-	private IProject project;
 
 	@Override
 	public boolean exists(String fileName) {
@@ -114,14 +83,19 @@ public class EclipseFileWriter implements IFileWriter {
 
 	@Override
 	public void write(String fileName, InputStream source) {
+		IFile file = project.getFile(fileName);
 		try {
-			IFile file = project.getFile(fileName);
+			IFileStore store = EFS.getStore(file.getLocationURI());
+			String localName = store.fetchInfo().getName();
+			if (!file.getName().equals(localName)) {
+				// case difference!
+				file = project.getFile(localName);
+			}
+
 			if (file.exists()) {
 				file.setContents(source, true, true, null);
 			} else {
-				if (!file.getParent().exists()) {
-					createFolder((IFolder) file.getParent());
-				}
+				ensureExists(file.getParent());
 				file.create(source, true, null);
 			}
 		} catch (CoreException e) {
