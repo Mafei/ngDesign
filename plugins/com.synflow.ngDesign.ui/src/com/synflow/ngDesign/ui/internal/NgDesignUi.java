a34886152e31e6089dbab3c988a30661ce6a35e8
Matthieu Wipliez <matthieu.wipliez@synflow.com>
2014-09-30 17:37:04 +0200

    set default theme on Windows when starting ngDesign

--- plugins/com.synflow.ngDesign.ui/src/com/synflow/ngDesign/ui/internal/NgDesignUi.java
+++ plugins/com.synflow.ngDesign.ui/src/com/synflow/ngDesign/ui/internal/NgDesignUi.java
@@ -10,13 +10,21 @@
  *******************************************************************************/
 package com.synflow.ngDesign.ui.internal;
 
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.e4.ui.css.swt.theme.ITheme;
+import org.eclipse.e4.ui.css.swt.theme.IThemeEngine;
+import org.eclipse.e4.ui.css.swt.theme.IThemeManager;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
 /**
  * The activator class controls the plug-in life cycle
  */
+@SuppressWarnings("restriction")
 public class NgDesignUi extends AbstractUIPlugin {
 
 	public static final String COMMAND_TYPE_EXPORT_TYPE = "com.synflow.ngDesign.ui.exportType";
@@ -37,14 +45,12 @@ public class NgDesignUi extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Creates and returns a new image descriptor for an image file located
-	 * within this plug-in.
+	 * Creates and returns a new image descriptor for an image file located within this plug-in.
 	 * 
 	 * @param path
-	 *            the relative path of the image file, relative to the root of
-	 *            the plug-in; the path must be legal
-	 * @return an image descriptor, or <code>null</code> if no image could be
-	 *         found
+	 *            the relative path of the image file, relative to the root of the plug-in; the path
+	 *            must be legal
+	 * @return an image descriptor, or <code>null</code> if no image could be found
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
@@ -60,6 +66,31 @@ public class NgDesignUi extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+
+		// set default theme for Windows 8 (otherwise the product looks ugly)
+		ServiceReference<IThemeManager> reference;
+		reference = context.getServiceReference(IThemeManager.class);
+		try {
+			IThemeManager manager = context.getService(reference);
+			Display display = PlatformUI.getWorkbench().getDisplay();
+			IThemeEngine engine = manager.getEngineForDisplay(display);
+			ITheme themeActive = engine.getActiveTheme();
+			if (themeActive == null) {
+				if (Platform.OS_WIN32.equals(Platform.getOS())) {
+					final String versionWin7 = "6.1";
+					String osVersion = System.getProperty("os.version");
+					if (osVersion != null && osVersion.compareTo(versionWin7) > 0) {
+						// if OS is more recent than Windows 7, make theme API use Windows 7 theme
+						System.setProperty("os.version", versionWin7);
+					}
+
+					// set default theme
+					engine.setTheme("org.eclipse.e4.ui.css.theme.e4_default", true);
+				}
+			}
+		} finally {
+			context.ungetService(reference);
+		}
 	}
 
 	@Override
